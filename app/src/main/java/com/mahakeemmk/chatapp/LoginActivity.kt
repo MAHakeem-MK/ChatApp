b76210bfa8cbe76d3dd4fa4090f436de50363824
mahakeemmk <dev.mahakeemmk@gmail.com>
2019-04-19 15:43:17 +0530

    firebase authentication and users info in database implemented.

---
+++ app/src/main/java/com/mahakeemmk/chatapp/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.mahakeemmk.chatapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        signIn_button.setOnClickListener {
+            val email = email_signIn_edit.text.toString()
+            val password = password_signIn_edit.text.toString()
+
+            Log.d("sign in","Email:$email")
+
+            if (email.isEmpty()||password.isEmpty()) {
+                Toast.makeText(this,"please fill all the fields", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+                    Log.d("login","successful")
+                }.addOnFailureListener {
+                    Log.d("login","failed ${it.message}")
+                    Toast.makeText(this,it.message,Toast.LENGTH_LONG).show()
+                }
+        }
+    }
+}
---
+++ app/src/main/java/com/mahakeemmk/chatapp/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.mahakeemmk.chatapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.Duration
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth:FirebaseAuth
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser:FirebaseUser? = auth.currentUser
+        Log.d("register","already signed In")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        register_button.setOnClickListener {
+            val name = name_edit_text.text.toString()
+            val email = email_edit_text.text.toString()
+            val password = password_edit_text.text.toString()
+            Log.d("register","Name:$name")
+
+            if (email.isEmpty()||password.isEmpty()||name.isEmpty()) {
+                Toast.makeText(this,"please fill all the fields",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+
+                    Log.d("register","registration successful")
+                    val uid = auth.uid?:""
+                    val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+                    val user = User(uid,name)
+                    ref.setValue(user)
+                        .addOnSuccessListener {
+                            Log.d("register","name saved")
+                        }
+                }.addOnFailureListener {
+                    Toast.makeText(this,it.message,Toast.LENGTH_LONG).show()
+                }
+
+        }
+
+        textButton_signIn.setOnClickListener {
+            Log.d("register","you clicked sign in")
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
+
+class User(val uid:String,val name:String)
